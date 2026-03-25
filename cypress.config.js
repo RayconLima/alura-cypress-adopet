8a6995e701093bd7530fb9cfa7b1232f8b71908d
Raycon Batista <raycon.batista@systock.com.br>
2026-03-25 08:13:47 -0400

    feat: :sparkles: Add base URL configuration for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "9dx34q",
+  base_url: "https://adopet-tau.vercel.app",
   allowCypressEnv: false,
   viewportWidth: 1920,
   viewportHeight: 1080,
