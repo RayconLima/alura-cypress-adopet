af17f3379e7535268fdfbe8f7cf914a233f6bb4b
Raycon Batista <raycon.batista@systock.com.br>
2026-03-25 08:02:18 -0400

    feat: :sparkles: Adicionar workflow de CI do Cypress
    Adicionar workflow do GitHub Actions para executar testes do Cypress em paralelo com gravação no Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "9dx34q",
   allowCypressEnv: false,
-
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
