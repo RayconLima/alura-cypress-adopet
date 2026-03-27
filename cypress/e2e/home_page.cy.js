50b5cd5217874447fdab852b736f9ade7f7d7735
Raycon Batista <raycon.batista@systock.com.br>
2026-03-27 08:37:06 -0400

    feat: :sparkles: Add user registration tests and enhance home page tests for pet images visibility

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -13,4 +13,19 @@ describe('Home page', () => {
         cy.visit('https://adopet-tau.vercel.app/');
         cy.contains('Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should('be.visible');
     });
+
+    it('should display pets images on the main page.', () => {
+        cy.visit('https://adopet-tau.vercel.app/');
+        cy.contains('a', 'Ver pets disponíveis para adoção').click();
+
+        /**
+         * todo: caso não haja outros testes referentes ao conjuntos de image dos pets, 
+         * pode ser interessante validar outras propriedades como texto alternativo, titulo, etc.
+        */
+
+        cy.wait(3000);
+        cy.get(".cards").each(($el) => {
+            cy.wrap($el).find('img').should('be.visible');
+        });
+    });
 });
